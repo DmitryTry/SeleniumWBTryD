424aa719dc022779bb9211f3878302bd6c68059d
DmitryTry <dmitrytrunevsky@coherentsolutions.com>
2022-12-06 16:05:20 +0300

    task60 pageFactory with Singleton was added

---
+++ task-60/src/test/java/com/issoft/training/pageFactory/helpers/YandexInboxPage.java
@@ -0,0 +1,35 @@
+package com.issoft.training.pageFactory.helpers;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class YandexInboxPage {
+
+    @FindBy(xpath = "//div[@data-key='box=left-box']")
+    private WebElement messageBox;
+
+    @FindBy(xpath = "//span[@class='user-account__name']/following-sibling::div")
+    private WebElement userMenu;
+
+    @FindBy(xpath = "//a[contains(@class, 'item_action_exit')]")
+    private WebElement logoutButton;
+
+    public YandexInboxPage() {
+        WebDriver driver = WebDriverSingleton.getInstance().getDriver();
+        PageFactory.initElements(driver, this);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOf(messageBox));
+    }
+
+    public void logout() {
+        userMenu.click();
+        logoutButton.click();
+        new YandexSignInPage();
+    }
+}
