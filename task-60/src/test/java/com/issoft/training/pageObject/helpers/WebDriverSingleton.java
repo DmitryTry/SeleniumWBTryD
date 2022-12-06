12d43a3cee71bdb273fafd3fda5c0becaaa7e09a
DmitryTry <dmitrytrunevsky@coherentsolutions.com>
2022-12-06 16:41:08 +0300

    task60 pageObject pages and singleton was added

--- task-60/src/test/java/com/issoft/training/pageObject/helpers/WebDriverSingleton.java
+++ task-60/src/test/java/com/issoft/training/pageObject/helpers/WebDriverSingleton.java
@@ -1,4 +1,41 @@
 package com.issoft.training.pageObject.helpers;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class WebDriverSingleton {
+    private WebDriver driver;
+    private static WebDriverSingleton webDriverSingleton;
+
+    private WebDriverSingleton() {
+
+    }
+
+    public static WebDriverSingleton getInstance() {
+        if (webDriverSingleton == null) {
+            webDriverSingleton = new WebDriverSingleton();
+        }
+        return webDriverSingleton;
+    }
+
+    public WebDriver getDriver() {
+        if (driver == null) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+            return driver;
+        }
+        return driver;
+    }
+
+    public void driverClose() {
+        if (webDriverSingleton != null) {
+            driver.close();
+            webDriverSingleton = null;
+        }
+    }
 }
